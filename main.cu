62d8d7121b685906bca13744067a44a10a6aa781
Chi-kwan Chan <ckch@nordita.org>
2011-03-06 15:02:09 -0800

    A home-made argument parser

--- main.cu
+++ main.cu
@@ -2,6 +2,8 @@
 #include <math.h>
 #include "ihd.h"
 
+#define HAS_ARG (i+1 < argc && argv[i+1][0] != '-')
+
 R noise(R x, R y)
 {
   return 0.5 - (R)rand() / RAND_MAX;
@@ -20,38 +22,62 @@ R KH(R x, R y)
 
 int main(int argc, char *argv[])
 {
-  const char rotor[] = "-/|\\";
-  const char input[] = "input.raw";
+  const char  rotor[] = "-/|\\";
+  const char *input   = NULL;
 
-  R nu = (argc > 1) ? atof(argv[1]) : 1.0e-5;
-  R mu = (argc > 2) ? atof(argv[2]) : 1.0e-2;
-  R fi = (argc > 3) ? atof(argv[3]) : 5.0e-2;
-  R ki = (argc > 4) ? atof(argv[4]) : 1.0e+2;
-  R tt = (argc > 5) ? atof(argv[5]) : 1024.0;
+  R nu = 1.0e-5, mu = 1.0e-2;
+  R fi = 5.0e-2, ki = 1.0e+2;
+  R tt = 1024.0, fo;
 
-  Z n0 = (argc > 6) ? atoi(argv[6]) : 1024;
-  Z n1 = (argc > 7) ? atoi(argv[7]) : 1024;
-  Z n2 = (argc > 8) ? atoi(argv[8]) : 1024;
-
-  R fo = 5 * n1 * n2 * (21.5 + (fi * ki > 0.0 ? 0 : 8) +
-                        12.5 * (log2((double)n1) + log2((double)n2)));
-  Z i  = 0;
+  Z n0 = 1024, n1 = 1024, n2 = 1024, i;
 
   cudaEvent_t t0, t1;
   cudaEventCreate(&t0);
   cudaEventCreate(&t1);
 
-  printf("2D spectral hydrodynamic code with CUDA\n");
-  setup(n1, n2);
+  printf("2D spectral hydrodynamic code with CUDA\n\n");
 
-  if(exist(input))
-    scale(forward(W, load(w, input)), 1.0 / (n1 * n2));
-  else {
-    scale(forward(W, init(w, noise)), 1.0 / (n1 * n2));
-    dump(name(i), inverse(w, W));
+  for(i = 1; i < argc; ++i) {
+    /* Arguments do not start with '-' are input file names */
+    if(argv[i][0] != '-')
+      input = argv[i];
+    /* Arguments start with '-' are options */
+    else switch(argv[i][1]) {
+      case 'n': if(HAS_ARG) { nu = atof(argv[++i]); break; }
+      case 'm': if(HAS_ARG) { mu = atof(argv[++i]); break; }
+      case 'f': if(HAS_ARG) { fi = atof(argv[++i]); break; }
+      case 'k': if(HAS_ARG) { ki = atof(argv[++i]); break; }
+      case 'T': if(HAS_ARG) { tt = atof(argv[++i]); break; }
+      case 'N': if(HAS_ARG) { n0 = atoi(argv[++i]);
+                if(HAS_ARG)   n1 = atoi(argv[++i]);
+                if(HAS_ARG)   n2 = atoi(argv[++i]); break; }
+      default : printf("Ignore \"%s\"\n", argv[i]);
+    }
   }
 
-  while(i++ < n0) {
+  printf("Dissipation :\tnu = %g,\tmu = %g\n", nu, mu);
+  printf("Forcing     :\tfi = %g,\tki = %g\n", fi, ki);
+  printf("Time        :\ttt = %g,\tnt = %d\n", tt, n0);
+  printf("Resolution  :\tn1 = %d,\tn2 = %d\n", n1, n2);
+  printf("Input/init  :\t\"%s\", ", input ? input : "none");
+
+  setup(n1, n2);
+  fo = 5 * n1 * n2 * (21.5 + (fi * ki > 0.0 ? 0 : 8) +
+                      12.5 * (log2((double)n1) + log2((double)n2)));
+
+  if(input && exist(input)) {
+    scale(forward(W, load(w, input)), 1.0 / (n1 * n2));
+    printf("LOADED\n");
+  } else {
+    if(input) printf("FAILED TO LOAD, ");
+    scale(forward(W, init(w, noise)), 1.0 / (n1 * n2));
+    printf("initialized with noise\n");
+    dump(name(0), inverse(w, W));
+  }
+
+  printf("\nStart simulation...\n");
+
+  for(i = 1; i <= n0; ++i) {
     float ms;
     Z ns = (Z)ceil(tt / n0 / 0.9 / getdt(1.0, nu, mu)), j;
     R dt =         tt / n0 / ns;
